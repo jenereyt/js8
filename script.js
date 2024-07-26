3740991235f5003a17d623bb87bb278aa2b5082e
jenereyt S <jonibek.usa@gmail.com>
2024-07-26 16:44:10 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// let nums = [1,2,3,4,5,6,7,8,9]
+
+// let evens = []
+// let odds = []
+
+// nums.filter((Number) => {
+//     if(Number % 2 === 0){
+//         evens.push(Number)
+//     }else{
+//         odds.push(Number)
+//     }
+// })
+// console.log({evens,
+// odds
+// });
+let shuffled = [1, 2, 3, 4]
+let total = 0
+let average = 0
+shuffled.forEach(value => {
+  total += value
+});
+console.log(total)
+console.log(average)
